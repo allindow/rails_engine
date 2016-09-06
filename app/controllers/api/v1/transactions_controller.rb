92450c9076030c098fa2f44a9d5c6fe47ec6f21b
Matthew Campbell <ematthewcampbell@gmail.com>
2016-09-06 13:44:40 -0600

    make index and show actions for transactions controller

---
+++ app/controllers/api/v1/transactions_controller.rb
@@ -0,0 +1,10 @@
+class Api::V1::TransactionsController < ApplicationController
+
+  def index
+    @transactions = Transaction.all
+  end
+
+  def show
+    @transaction = Transaction.find(params[:id])
+  end
+end
