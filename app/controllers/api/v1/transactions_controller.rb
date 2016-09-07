82138bc034c161f3f9996450e5cfb7c2aa143403
Matthew Campbell <ematthewcampbell@gmail.com>
2016-09-06 22:47:00 -0600

    created controllers

--- app/controllers/api/v1/transactions_controller.rb
+++ app/controllers/api/v1/transactions_controller.rb
@@ -1,10 +1,14 @@
 class Api::V1::TransactionsController < Api::V1::BaseController
 
   def index
-    @transactions = Transaction.all
+    transactions = Transaction.all
+
+    respond_with transactions
   end
 
   def show
-    @transaction = Transaction.find(params[:id])
+    transaction = Transaction.find(params[:id])
+
+    respond_with transaction
   end
 end
