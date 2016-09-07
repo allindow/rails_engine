84705094760af0c5d62251ed889d42c3ba1785c8
Angela Lindow <angelalindow@gmail.com>
2016-09-06 20:57:47 -0600

    Add random search endpoints add merchants most revenue

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,4 +1,13 @@
 class Merchant < ApplicationRecord
   has_many :items
   has_many :invoices
+  has_many :invoice_items, through: :invoices
+
+  def self.most_revenue(number)
+    select("merchants.*, SUM(invoice_items.quantity*invoice_items.unit_price) AS revenue")
+    .joins(:invoice_items)
+    .order("revenue DESC")
+    .group("merchants.id")
+    .take(number)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,23 +6,26 @@ Rails.application.routes.draw do
       resources :items, only: [:index, :show] do
         get 'find', on: :collection, to: "items/search#show"
         get 'find_all', on: :collection, to: "items/search#index"
+        get 'random', on: :collection, to: "items/random#show"
       end
 
       resources :invoices, only: [:index, :show] do
         get 'find', on: :collection, to: "invoices/search#show"
         get 'find_all', on: :collection, to: "invoices/search#index"
+        get 'random', on: :collection, to: "invoices/random#show"
       end
 
       resources :invoice_items, only: [:index, :show] do
         get 'find', on: :collection, to: "invoice_items/search#show"
         get 'find_all', on: :collection, to: "invoice_items/search#index"
-
+        get 'random', on: :collection, to: "invoice_items/random#show"
       end
 
       resources :merchants, only: [:index, :show] do
         get 'find', on: :collection, to: "merchants/search#show"
         get 'find_all', on: :collection, to: "merchants/search#index"
         get 'random', on: :collection, to: "merchants/random#show"
+        get 'most_revenue', on: :collection, to: "merchants/most_revenue#index"
       end
 
       resources :customers, only: [:index, :show]
