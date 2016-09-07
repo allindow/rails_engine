a582d9d7d1f0eacbd5b726b382db59eea38fdecf
Angela Lindow <angelalindow@gmail.com>
2016-09-07 14:46:44 -0600

    Implement endpoint for merchant pending customers

--- app/controllers/api/v1/merchants/pending_customers_controller.rb
+++ app/controllers/api/v1/merchants/pending_customers_controller.rb
@@ -1,4 +1,6 @@
-class Api::V1::Merchants::PendingCustomers < Api::V1::BaseController
+class Api::V1::Merchants::PendingCustomersController < Api::V1::BaseController
   def index
+    customers = Merchant.find(params[:id]).pending_customers
+    respond_with customers
   end
 end
--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -6,16 +6,28 @@ RSpec.describe Merchant, type: :model do
   it { should have_many(:invoices) }
 
   it "can get merchants with most revenue" do
-    4.times do
-      create(:invoice_item)
-    end
+    create_list(:invoice_item, 4)
 
     result = Merchant.select("merchants.*, SUM(invoice_items.quantity*invoice_items.unit_price) AS revenue")
     .joins(:invoice_items)
     .order("revenue DESC")
     .group("merchants.id")
     .take(2)
-    
+
     expect(Merchant.most_revenue(2)).to eq(result)
+  end
+
+  it "can get pending customers for merchant" do
+    merchant = create(:merchant)
+    invoice = create(:invoice, merchant_id: merchant.id)
+    create(:transaction, invoice_id: invoice.id)
+    create(:transaction, invoice_id: invoice.id, result: "failed")
+
+    result = merchant.customers.find(merchant.invoices
+              .includes(:transactions).references(:transactions)
+              .where.not(transactions: { result: "success" })
+              .pluck(:customer_id))
+
+    expect(merchant.pending_customers).to eq(result)
   end
 end
