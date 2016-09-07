185875ea2652df208a535ae74ed4d17812de2342
Matthew Campbell <ematthewcampbell@gmail.com>
2016-09-06 23:14:59 -0600

    only one test failing

--- app/controllers/api/v1/customers/search_controller.rb
+++ app/controllers/api/v1/customers/search_controller.rb
@@ -1,12 +1,12 @@
 class Api::V1::Customers::SearchController < Api::V1::BaseController
   def index
-    customers = Customers.where(query_params)
+    customers = Customer.where(query_params)
 
     respond_with customers
   end
 
   def show
-    customer = Customers.find_by(query_params)
+    customer = Customer.find_by(query_params)
 
     respond_with customer
   end
